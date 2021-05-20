f78ca4c5e1e7abe238584b1fa782ede3cc0171fe
tirtharaj101 <59131975+tirtharaj101@users.noreply.github.com>
2021-05-20 20:51:49 +0530

    new

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ mysql = MySQL(app)
 @app.route('/')
 def home():
     return render_template("home.html")
-@app.route('/dash')
-def dash():
-    return render_template("dashboard.html")
  
 @app.route('/register', methods=["GET", "POST"]) 
 def register():
@@ -51,14 +48,43 @@ def login():
             if bcrypt.hashpw(password, user["password"].encode('utf-8')) == user["password"].encode('utf-8'):
                 session['name'] = user['name']
                 session['email'] = user['email']
-                return render_template("dashboard.html")
+                return redirect("index")
             else:
                 return "Error password and email not match"
         else:
             return "Error user not found"
     else:
         return render_template("login.html")
+@app.route('/index',methods=["GET","POST"])
+def index():
+    if request.method=='GET':
+        cur2=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        s="SELECT * FROM employee"
+        cur2.execute(s)
+        output_data=cur2.fetchall()
+        cur2.close()
+        return render_template("index.html", output_data=output_data, s=s)
+    else:
+        name=request.form['name']
+        desig=request.form['designation']
+        salary=request.form['salary']
+@app.route('/create',methods=["GET","POST"])
+def create():
+        if request.method == 'GET':
+            return render_template("create.html")
+        else:
+            name = request.form['name']
+            designation = request.form['designation']
+            salary = request.form['salary']
  
+            cur3 = mysql.connection.cursor()
+            cur3.execute("INSERT INTO employee (name, designation, salary) VALUES (%s,%s,%s)"(name,designation,salary))
+            mysql.connection.commit()
+            session['name'] = request.form['name']
+            session['designation'] = request.form['designation']
+            session['salary'] = request.form['salary']
+            return redirect(url_for('login'))
+
 @app.route('/logout')
 def logout():
     session.clear()
